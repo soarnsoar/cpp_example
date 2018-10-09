eeee5d6d18275db4d38339057bb5cddc43a0a6d1
jhchoi <soarnsoar@gmail.com>
2018-10-09 18:47:10 +0200

    Update move_statbox.cpp

--- move_statbox.cpp
+++ move_statbox.cpp
@@ -1,3 +1,4 @@
- TPaveStats *st = ((TPaveStats *)(gPad->GetPrimitive("stats")));
+//After draw a histogram
+TPaveStats *st = ((TPaveStats *)(gPad->GetPrimitive("stats")));
   st->SetY1NDC(st->GetY1NDC() - 0.7);
   st->SetY2NDC(st->GetY2NDC() - 0.7);
