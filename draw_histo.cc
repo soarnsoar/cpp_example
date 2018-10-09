f99c33d58ede494f73d45cf3720dbb4704f9ee9b
your name your last name <soarnsoar@gmail.com>
2018-10-10 05:48:31 +0900

    add draw_histo

--- draw_histo.cc
+++ draw_histo.cc
@@ -27,6 +27,8 @@
 
 
 #include <iostream>
+#include <TPaveStats.h>
+
 using namespace std;
 class sample_info {
 private:
@@ -34,6 +36,7 @@ private:
   TString _mass, _lepton,_addvirtual,_pythiaPhoton,_showerL,_photos;
   vector<TString> _vhistoname;
   vector<int> _vcolor; 
+  vector<int> _vlinestyle; 
  
   vector<TString> _vlegTL;
   vector<TString> _vlegTR;
@@ -60,7 +63,7 @@ public:
   void Clear_histos();
   void Delete_histo();
   void Draw_histos();
-  void Compare_histos(TString _name_);
+  void Compare_histos(TString _name_,TString _axis_title_);
 
 
   void Set_mass(TString mass);
@@ -99,7 +102,12 @@ sample_info::sample_info(void){
   _vcolor.push_back(3);
   _vcolor.push_back(1);
   _vcolor.push_back(7);
-  _vcolor.push_back(8);   
+  _vcolor.push_back(8); 
+  
+  _vlinestyle.push_back(1);
+  _vlinestyle.push_back(9);
+  _vlinestyle.push_back(1);
+  _vlinestyle.push_back(9);
 }
 //TString sample_info::Get_Xname(){
 // return _histoname;//
@@ -205,11 +213,23 @@ void sample_info::Draw_histos(){
     c->SaveAs(dirname+"/"+vhisto[ih]->GetName()+".png");
     
   }
+  
+  
 }
 
-void sample_info::Compare_histos(TString _name_){
+void sample_info::Compare_histos(TString _name_,TString _axis_title_){
 
     TCanvas *c = new TCanvas();
+    c->Clear();
+    c->Divide(1,2);
+    c->cd(1);
+    gPad->SetPad(0.05, 0.3, 0.95, 0.95);
+    
+    c->cd(2);
+    gPad->SetPad(0.05,0.05,0.95,0.3);
+    gPad->SetBottomMargin(0.3);
+
+
     double x1=0.9,y1=0.85,x2=0.45,y2=0.7;
 
     /*    if(_name_.Contains("h_dRboson")||_name_.Contains("h_dPhiboson")){//RrightBottom
@@ -250,52 +270,139 @@ void sample_info::Compare_histos(TString _name_){
     for(int ih=0; ih<vhisto.size(); ih++){
       TH1D *h=(TH1D*)vhisto[ih]->Clone();
       h->Scale(1/h->Integral());
+      cout<<"hnorm="<<h->Integral()<<endl;
       if((h->GetMinimum() > 0)&&(ih==0)) min = h->GetMinimum();
       double max_i=h->GetMaximum();
       double min_i=h->GetMinimum();
       if(max_i>max) max=max_i;
       if((min_i<min)&&(min_i>0)) min=min_i;
+      
     } 
-    
+    TH1D *hdefault;;    
     for(int ih=0; ih<vhisto.size(); ih++){
       TH1D *h=(TH1D*)vhisto[ih]->Clone();
+      TH1D *hratio;
       h->Sumw2();
       h->Scale(1/h->Integral());
+      h->SetLineColor(_vcolor[ih]);
+      h->SetMarkerColor(_vcolor[ih]);
+
       if(ih==0){
+	
+	hdefault=(TH1D*)h->Clone();
+	c->cd(1);
 	h->Draw("E");
-	if(_name_.Contains("Nphoton") && !_name_.Contains("fsr")){
-	  h->GetXaxis()->SetRangeUser(0,500);
+	h->SetStats(0);
 
-	}
-	else if(_name_.Contains("h_PT")){
-          h->GetXaxis()->SetRangeUser(0,50);
-
-        }
-	else if(_name_.Contains("h_Ephoton_fsr")){
-          h->GetXaxis()->SetRangeUser(0,5);
-
-        }
-	else if(_name_.Contains("h_Nphoton_fsr")){
-          h->GetXaxis()->SetRangeUser(-1,3);
-
-        }
-
-	else if(_name_.Contains("10to50")&&_name_.Contains("h_M_boson")){
-          h->GetXaxis()->SetRangeUser(10,50);	  
-	}
-	else if(_name_.Contains("50plus")&&_name_.Contains("h_M_boson")){
-          h->GetXaxis()->SetRangeUser(50,500);
-	}
-	else if(_name_.Contains("200to400")&&_name_.Contains("h_Mboson")){
-          h->GetXaxis()->SetRangeUser(200,400);
-	}
 
       }
-      else h->Draw("sames E");
+      else{
+        c->cd(1);
+        h->Draw("sames E");
+	c->Update();
+	/*
+	TPaveStats *st = ((TPaveStats *)(c->cd(1)->GetPrimitive("stats")));
+	st->SetY1NDC(st->GetY1NDC() - 0.7*ih);
+	st->SetY2NDC(st->GetY2NDC() - 0.7*ih);
+	*/
+	h->SetStats(0);
 
-      if((_name_.Contains("Nphoton") && !_name_.Contains("Nphoton_fsr"))||_name_.Contains("Ephoton")){
+
+
+
+	hratio=(TH1D*)h->Clone();
+        hratio->TH1D::Divide(hdefault);
+        c->cd(2);
+        hratio->Draw("sames E");
+        hratio->SetStats(0);
+	hratio ->SetTitle("");
+	hratio->SetMinimum(0.8);
+	hratio->SetMaximum(1.2);
+	hratio-> SetNdivisions(6, "Y");
+	hratio->SetStats(0);
+	hratio -> GetXaxis() -> SetLabelSize(0.13);
+	hratio -> GetYaxis() -> SetLabelSize(0.13);
+	hratio -> GetYaxis() -> SetTitleSize(0.15);
+	hratio -> GetXaxis() -> SetTitleSize(0.15);
+	hratio -> GetYaxis() -> SetTitleOffset(0.3);
+	hratio -> GetXaxis() -> SetTitleOffset(0.8);
+	hratio -> GetYaxis() -> SetTitle(_axis_title_);
+	//TLine *line = new TLine(hratio->GetXaxis()->GetXmin(),1,hratio->GetXaxis()->GetXmax(),1);
+        //line->SetLineColor(_vcolor[0]);
+	//line->Draw("sames");
+      }
+      if(_name_.Contains("Nphoton") && !_name_.Contains("fsr")){
+	c->cd(1);
+	h->GetXaxis()->SetRangeUser(0,500);
+		
+	
+	if(ih>0){
+	c->cd(2);
+	hratio->GetXaxis()->SetRangeUser(0,500);
+
+	}	
+      }
+      else if(_name_.Contains("h_PT12")||_name_.Contains("h_PT_boson")||_name_.Contains("PTpostpre")){
+	c->cd(1);
+	h->GetXaxis()->SetRangeUser(0,15);
+	//h->GetXaxis()->SetRangeUser(0,5);
+	if(ih>0){
+	c->cd(2);
+	//	hratio->GetXaxis()->SetRangeUser(0,50);
+	hratio->GetXaxis()->SetRangeUser(0,15);
+	}
+      }
+      else if(_name_.Contains("h_Ephoton")){
+	c->cd(1);
+	//	h->GetXaxis()->SetRangeUser(0,5);
+	h->GetXaxis()->SetRangeUser(0,1);
+	if(ih>0){
+	c->cd(2);
+	//	hratio->GetXaxis()->SetRangeUser(0,5);
+	hratio->GetXaxis()->SetRangeUser(0,1);
+	}
+      }
+      //	else if(_name_.Contains("h_Nphoton_fsr")){
+      //          h->GetXaxis()->SetRangeUser(0,10);
+      
+      //}
+      
+      else if(_name_.Contains("10to50")&&(_name_.Contains("h_M_boson")||_name_.Contains("h_M12")||_name_.Contains("Mpostpre"))){
+	c->cd(1);
+	//	h->GetXaxis()->SetRangeUser(10,50);	  
+	h->GetXaxis()->SetRangeUser(5,55);	  
+	if(ih>0){
+	  c->cd(2);
+	  //	  hratio->GetXaxis()->SetRangeUser(10,50);	  
+	  hratio->GetXaxis()->SetRangeUser(5,55);	  
+	}
+	}
+      else if(_name_.Contains("50plus")&&(_name_.Contains("h_M_boson")||_name_.Contains("h_M12")||_name_.Contains("Mpostpre"))){
+	c->cd(1);
+	//	h->GetXaxis()->SetRangeUser(50,200);
+	h->GetXaxis()->SetRangeUser(80,100);
+	if(ih>0){
+	c->cd(2);
+	//	hratio->GetXaxis()->SetRangeUser(50,200);
+	hratio->GetXaxis()->SetRangeUser(80,100);
+	}
+	}
+      else if(_name_.Contains("200to400")&&(_name_.Contains("h_Mboson")||_name_.Contains("h_M12")||_name_.Contains("Mpostpre"))){
+	c->cd(1);
+	//	h->GetXaxis()->SetRangeUser(200,400);
+	h->GetXaxis()->SetRangeUser(190,410);
+	if(ih>0){
+	c->cd(2);
+	//	hratio->GetXaxis()->SetRangeUser(200,400);
+	hratio->GetXaxis()->SetRangeUser(190,410);
+	}
+	}
+      
+      
+
+      if((_name_.Contains("Nphoton") && !_name_.Contains("Nphoton_fsr")) || _name_.Contains("Ephoton")||(_name_.Contains("h_M_boson")||_name_.Contains("h_M12"))){
 	c->SetLogy();
-	if(min==0) min=0.1;     
+	
       }
       if(_name_.Contains("Nphoton_fsr")){
 	cout<<_name_<<endl;
@@ -304,40 +411,57 @@ void sample_info::Compare_histos(TString _name_){
       //      else if(_name_.Contains("Ephoton")) c->SetLogy();
 
       
-  
-      h->GetYaxis()->SetRangeUser(min,max);
-      //else h->GetYaxis()->SetRangeUser(0.1,max);
-      //h->GetYaxis()->SetRangeUser(0.1,max);
-      //cout<<"max="<<max<<endl;
-      //cout<<"min"<<min<<endl;
+      double ymax=max+0.1*(max-min);
+      double ymin=min-0.1*(max-min);
+      if(max<0 || max==0) cout<<"max="<<max<<endl;
+      if(c->cd(1)->GetLogy()==1){
+	if(min==0 || min<0) min=max*0.0001;     
+	c->cd(1);
+	h->GetYaxis()->SetRangeUser(min,ymax);
+      }
+      else{
+	c->cd(1);
+	h->GetYaxis()->SetRangeUser(ymin,ymax);
+      }
+
+
+
            
       leg->TLegend::AddEntry(h,vhisto[ih]->GetName());
-      h->SetLineColor(_vcolor[ih]);
-      h->SetMarkerColor(_vcolor[ih]);
-      // h->SetMarkerStyle(0);
-      h->SetStats(0);
       
-    }
+      if(_vlinestyle[ih]) h->SetLineStyle(_vlinestyle[ih]);
+      // h->SetMarkerStyle(0);
+      // h->SetStats(0);
+      //###//
+
+      ///
+
+
+    }//end of for ih
+    c->cd(1);
 
     leg->Draw();
+
+    c->cd(2);
+    c->cd(2)->SetGrid();
     
     c->SaveAs(_name_);
 }
 void draw_histo_all(){
-    //h_dR1
+  //h_dR1
   /*
-  void Add_histo(TString _histoname_);
-  void Load_histos();
+    void Add_histo(TString _histoname_);
+    void Load_histos();
 
-  void Set_mass(TString mass);
-  void Set_lepton(TString lepton);
-  void Set_addvirtual(TString addvirtual);
-  void Set_pythiaPhoton(TString pythiaPhoton);
-  void Set_showerL(TString showerL);
-  void Set_photos(TString photos);
+    void Set_mass(TString mass);
+    void Set_lepton(TString lepton);
+    void Set_addvirtual(TString addvirtual);
+    void Set_pythiaPhoton(TString pythiaPhoton);
+    void Set_showerL(TString showerL);
+    void Set_photos(TString photos);
 
-  void Set_filename();
-   */
+    void Set_filename();
+  */
   vector<TString> vlep; vlep.push_back("muon");vlep.push_back("electron");
   vector<TString> vmass; vmass.push_back("10to50");vmass.push_back("50plus");vmass.push_back("200to400");
   vector< vector<TString> > vsample;
@@ -405,6 +529,9 @@ void draw_histo_all(){
 	
 	mysample.Add_histo("h_dR12");
 	mysample.Add_histo("h_dPhi12");
+        mysample.Add_histo("h_PT12");
+        mysample.Add_histo("h_M12");
+
 	mysample.Add_histo("h_M_boson");
 	mysample.Add_histo("h_PT_boson");
 	mysample.Add_histo("h_PT_lep1");
@@ -422,7 +549,88 @@ void draw_histo_all(){
   
 }
 
+void draw_postpre_compare_histos(){
+  vector<TString> vlep; vlep.push_back("muon");vlep.push_back("electron");
+  vector<TString> vmass; vmass.push_back("10to50");vmass.push_back("50plus");vmass.push_back("200to400");
+  vector< vector<TString> > vsample;
+  vector<TString> apsp;//addvirtual/pythiaPhoton//showerL/photos/                                                                                              
 
+  apsp.clear();
+  apsp.push_back("Z");
+  apsp.push_back("off");
+  apsp.push_back("off");
+  apsp.push_back("on");
+  vsample.push_back(apsp);
+
+  apsp.clear();
+  apsp.push_back("gamma");
+  apsp.push_back("off");
+  apsp.push_back("off");
+  apsp.push_back("on");
+  vsample.push_back(apsp);
+
+  apsp.clear();
+  apsp.push_back("off");
+  apsp.push_back("off");
+  apsp.push_back("off");
+  apsp.push_back("on");
+  vsample.push_back(apsp);
+
+  apsp.clear();
+  apsp.push_back("off");
+  apsp.push_back("on");
+  apsp.push_back("on");
+  apsp.push_back("off");
+  vsample.push_back(apsp);
+
+
+  for(int il=0; il<vlep.size(); il++){
+    for(int im=0; im<vmass.size(); im++){
+      for(int is=0;is<vsample.size();is++){
+	sample_info mysample;
+
+	
+	
+	mysample.Set_lepton(vlep[il]);
+	mysample.Set_mass(vmass[im]);
+	mysample.Set_addvirtual(vsample[is][0]);
+	mysample.Set_pythiaPhoton(vsample[is][1]);
+	mysample.Set_showerL(vsample[is][2]);
+	mysample.Set_photos(vsample[is][3]);
+	
+	
+	mysample.Set_filename();
+	mysample.Add_histo("h_M_boson");
+	mysample.Add_histo("h_M12");
+	if(vmass[im]=="50plus"){
+
+          mysample.Add_LegendTL("Mpostpre");
+        }
+  
+	mysample.vhisto[0]->SetName("M(preFSR)=M(DY boson)");
+	mysample.vhisto[1]->SetName("M(postFSR)=M(ll)");
+	TString dirname="compare_histo/"+vlep[il]+"/"+vmass[im]+"/Mpostpre_fsr/";
+	//vsample[is][0]+"_"+vsample[is][1]+"_"+vsample[is][2]+"_"+vsample[is][3]+"/";
+	gSystem->Exec("mkdir -p "+dirname);
+	mysample.Compare_histos(dirname+vsample[is][0]+"_"+vsample[is][1]+"_"+vsample[is][2]+"_"+vsample[is][3]+".pdf","post/pre");
+
+
+
+	mysample.Clear_histos();	
+	mysample.Add_histo("h_PT_boson");
+        mysample.Add_histo("h_PT12");
+	mysample.vhisto[0]->SetName("PT(preFSR)=PT(DY boson)");
+        mysample.vhisto[1]->SetName("PT(postFSR)=PT(ll)");
+	dirname="compare_histo/"+vlep[il]+"/"+vmass[im]+"/PTpostpre_fsr/";
+	gSystem->Exec("mkdir -p "+dirname);
+	mysample.Compare_histos(dirname+vsample[is][0]+"_"+vsample[is][1]+"_"+vsample[is][2]+"_"+vsample[is][3]+".pdf","post/pre");
+	
+	
+      }
+    }
+  }
+
+}
 void draw_compare_histos(){
   vector<TString> vlep; vlep.push_back("muon");vlep.push_back("electron");
   vector<TString> vmass; vmass.push_back("10to50");vmass.push_back("50plus");vmass.push_back("200to400");
@@ -450,6 +658,10 @@ void draw_compare_histos(){
 
   vX.push_back("h_dR12");
   vX.push_back("h_dPhi12");
+  vX.push_back("h_PT12");
+  vX.push_back("h_M12");
+
+
   vX.push_back("h_M_boson");
   vX.push_back("h_PT_boson");
   vX.push_back("h_PT_lep1");
@@ -466,7 +678,10 @@ void draw_compare_histos(){
 	//TL                                                                                                                                                                                                                                 
 	mysamples.Add_LegendTL("h_dR12");
 	mysamples.Add_LegendTL("h_dPhi12");
-
+	if(vmass[im]=="50plus"){
+	  mysamples.Add_LegendTL("h_dM_boson");
+	  mysamples.Add_LegendTL("h_dM12");
+	}
 	//TR                                                                                                                    
 	mysamples.Add_LegendTR("h_Nphoton_fsr");                                                                                                           
 	
@@ -475,12 +690,14 @@ void draw_compare_histos(){
 	//	mysamples.Add_LegendBR("h_dRboson_fsr");
 	mysamples.Add_LegendBR("h_dPhiboson");
 	//	mysamples.Add_LegendBR("h_dPhiboson_fsr");
+	if(vlep[il]=="muon"){
 	mysamples.Add_LegendBR("h_dR1.");
 	mysamples.Add_LegendBR("h_dR1_");
 	mysamples.Add_LegendBR("h_dR2");
 	mysamples.Add_LegendBR("h_dPhi1.");
 	mysamples.Add_LegendBR("h_dPhi1_");
 	mysamples.Add_LegendBR("h_dPhi2");
+	}
 	mysamples.Add_LegendBR("Nphoton.");
 
 
@@ -538,7 +755,7 @@ void draw_compare_histos(){
 	*/
 	TString dirname="compare_histo/"+vlep[il]+"/"+vmass[im]+"/";
 	gSystem->Exec("mkdir -p "+dirname);
-	mysamples.Compare_histos(dirname+"/"+vX[ix]+".pdf");
+	mysamples.Compare_histos(dirname+"/"+vX[ix]+".pdf","N/N(PYTHIA8)");
 
 
       }            
@@ -551,4 +768,6 @@ void draw_compare_histos(){
 
 void draw_histo(){
   draw_compare_histos();
+  draw_histo_all();
+  draw_postpre_compare_histos();
 }
