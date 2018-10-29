a7737d524ef887b3edc74aeeff3a17dd2c881f62
jhchoi <soarnsoar@gmail.com>
2018-10-29 13:36:52 +0100

    Create get_tfile_list.cc

---
+++ get_tfile_list.cc
@@ -0,0 +1,15 @@
+void loopdir() {
+   TFile *f1 = TFile::Open("hsimple.root");
+   TIter keyList(f1->GetListOfKeys());
+   TKey *key;
+   TCanvas c1;
+   c1.Print("hsimple.ps[");
+   while ((key = (TKey*)keyList())) {
+      TClass *cl = gROOT->GetClass(key->GetClassName());
+      if (!cl->InheritsFrom("TH1")) continue;
+      TH1 *h = (TH1*)key->ReadObj();
+      h->Draw();
+      c1.Print("hsimple.ps");
+   }
+   c1.Print("hsimple.ps]");
+}
